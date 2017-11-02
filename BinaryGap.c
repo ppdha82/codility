a43aa94f0f6a40c3e69f7d6b46d75a49b3c638d3
ppdha82 <dh1982.park@hanwha.com>
2017-11-02 19:00:24 +0900

    modify to get 100 point -> changes shifting routine.    add flag to check to encounter number '1'

--- BinaryGap.c
+++ BinaryGap.c
@@ -1,14 +1,20 @@
-int solution(int N) {
+int solution(int N) 
+{
     // write your code in C99 (gcc 6.2.0)
     unsigned int dividened_number = N;
     int count_for_zero_length = 0;
     int max_length = 0;
-    
+    int flag_until_one = 0;
+
     while (dividened_number != 0)
     {
-        dividened_number >>= 1;
-        if (dividened_number & 0x1 == 0x1)
+        if (dividened_number & 0x1)
         {
+            if (flag_until_one == 0)
+            {
+                flag_until_one = 1;
+            }
+            
             if (max_length < count_for_zero_length)
             {
                 max_length = count_for_zero_length;
@@ -17,8 +23,14 @@ int solution(int N) {
         }
         else
         {
+            if (flag_until_one == 0)
+            {
+                dividened_number >>= 1;
+                continue;
+            }
             count_for_zero_length++;
         }
+        dividened_number >>= 1;
     }
     return max_length;
 }
