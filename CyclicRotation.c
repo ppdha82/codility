d78493bb5ca203f114d98665cfeb9c4332149350
ppdha82 <dh1982.park@hanwha.com>
2017-11-09 11:30:04 +0900

    Modify for 100 point. Array for buffer is filled by rotated result. And then become to copy to original Array.
    https://codility.com/demo/results/trainingQFWRKA-7FM/
    https://codility.com/demo/results/trainingA8GZ9J-Y7D/

--- CyclicRotation.c
+++ CyclicRotation.c
@@ -1,28 +1,31 @@
-void ShiftArrayBy(int* array, int array_length, int shift_index)
-{
-    int i;
-    int temp_buffer;
-    int shift_count;
-    
-    for (shift_count = 0;shift_count < shift_index;shift_count++)
-    {
-        temp_buffer = 0;
-        for (i = array_length - 1;i > 0;i--)
-        {
-            if (i == array_length - 1)
-            {
-                temp_buffer = array[i];
-            }
-            array[i] = array[i - 1];
-        }
-        array[0] = temp_buffer;
-    }
-}
-
 struct Results solution(int A[], int N, int K) {
     struct Results result;
     // write your code in C99 (gcc 6.2.0)
-    ShiftArrayBy(A, N, K);
+    int i;
+    int B[N];
+    int first_index_for_shift = N - (K % N);
+
+    if (N == 0)
+    {
+        return result;
+    }
+    else if ((N <= 1) || (first_index_for_shift == 0))
+    {
+        result.A = A;
+        result.N = N;
+        return result;
+    }
+    
+    for (i = 0;i < N;i++)
+    {
+        B[i] = A[(first_index_for_shift + i) % N];
+    }
+    
+    for (i = 0;i < N;i++)
+    {
+        A[i] = B[i];
+    }
+    
     result.A = A;
     result.N = N;
     return result;
