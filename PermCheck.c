111e0ca44485468d1fdc5ae4e597516316858023
Deukhyun Park <ppdha82@localhost.localdomain>
2017-11-27 22:46:51 +0900

    Add to use the algorithm that check whether is duplicated by using array for buffer or not

--- PermCheck.c
+++ PermCheck.c
@@ -1,23 +1,33 @@
 int solution(int A[], int N)
 {
     // write your code in C99 (gcc 6.2.0)
-    int total_of_sum = 0;
+    int B[N];
     int i = 0;
+    int total_of_sum = 0;
     
-    for (i = 1;i < N + 1;i++)
+    for (i = 0;i < N;i++)
     {
-        total_of_sum += i;
+        B[i] = 0;
+        total_of_sum += i + 1;
     }
     
     for (i = 0;i < N;i++)
     {
-        total_of_sum -= A[i];
+        printf("A[%d] = %d; B[A[%d]] = %d\n", i, A[i], i, B[A[i] - 1]);
+        if (A[i] > N || B[A[i] - 1] != 0)
+        {
+            return 0;
+        }
+        else
+        {
+            total_of_sum -= A[i];
+            B[A[i] - 1] = A[i];
+        }
     }
     
     if (total_of_sum != 0)
     {
         return 0;
     }
-    
     return 1;
 }
